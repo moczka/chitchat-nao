9320031ea53c761ab8035fdcdd5846704f44e8a4
capyBearista <capybearista@gmail.com>
2026-06-23 17:00:07 -0400

    update transcribe.py to test faster-whisper

--- transcribe.py
+++ transcribe.py
@@ -3,15 +3,18 @@ Name:   Javier S. Guerrero
 Date:   04/19/2026
 
 Transcribes audio to text
-
 '''
-import whisper
+from faster_whisper import WhisperModel
 
-model = whisper.load_model('tiny.en')
+model_size = "base.en"
 
+model = WhisperModel(model_size, device="cpu", compute_type="int8")
 
-result = model.transcribe('processed_audio_edilson.wav', language="en", 
-                          fp16=False)
+print("Starting transcription...")
 
-print(result["text"])
+segments, info = model.transcribe('processed_audio_edilson.wav', language="en", beam_size=5)
 
+print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+
+for segment in segments:
+    print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
