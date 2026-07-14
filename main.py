d702718a885919b4a20fa89fa1bcd41c68154933
moczka <steven.guesp@gmail.com>
2026-07-14 02:34:18 -0400

    Finalize modularizing transcriber

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+'''
+    Name:   Arjun Pramanik
+    Date:   June 9, 2026
+
+'''
+from transcribe import Transcribe
+from language_model import send_message
+
+WHISPER_MODEL = "base.en"
+
 def main():
-    print("Hello from chitchat-nao!")
+    # Set up transcribing tool
+    transcriber = Transcribe(model_name=WHISPER_MODEL)
+    transcriber.init()
+    transcriber.start()
+    # Prompt user
+    print('Listening... Ask Pazuzu anything.')
+    # Queue with user prompts
+    user_prompts = transcriber.get_transcriptions()
+
+    while True:
+        if (not user_prompts.empty()):
+            user_prompt = user_prompts.get()
+            print(f"User: {user_prompt}")
+            print('\nThinking...\n')
+            # Stop capturing audio while SLM generates an answer
+            transcriber.stop()
+            robot_resp = send_message(user_prompt)
+            print(f"\nRobot: {robot_resp}\n")
+            # Re-enable transcriber
+            transcriber.start()
+
 
 
 if __name__ == "__main__":
--- transcribe.py
+++ transcribe.py
@@ -17,13 +17,16 @@ CHUNK = 480 # To generate 30ms audio frames
 # Each audio frame is 30ms long so 30ms * 50 = roughly a second and a half of silence
 SILENCE_LENGTH = 50
 # Minimum number of frames containing speech.
-SPEECH_MIN_LENGTH = 40
-# State variables
-should_listen = True
+SPEECH_MIN_LENGTH = 20
 
 
 class Transcribe:
-    def __init__(self, model_name="base.en"):
+    def __init__(self, model_name="base.en", debug_on=False):
+        # Controls whether or not we print out debugging messages
+        self.__debug_on = debug_on
+        # Reference to audio stream & producer
+        self.__audio_stream = None
+        self.__audio_producer = None
         # Controls whether audio stream is opened or not.
         self.__should_listen = False
         # Stores the audio clips to be transcribed
@@ -31,12 +34,27 @@ class Transcribe:
         # Stores transcribed audio clips
         self.__transcribed_audio = queue.Queue()
         # Download model
-        print('Downloading Whisper model...')
+        self.__print('Downloading Whisper model...')
         try:
             self.__model = WhisperModel(model_name, device="cpu", compute_type="int8")
-            print('Whisper model downloaded successfully!')
+            self.__print('Whisper model downloaded successfully!')
         except:
-            print('Failed to download Whisper model.')
+            self.__print('Failed to download Whisper model.')
+
+    # Destructor
+    def __del__(self):
+        # Release I/O resource
+        if (self.__audio_stream != None and self.__audio_producer):
+            self.__audio_stream.close()
+            self.__audio_producer.terminate();
+
+    # Initializes capturing audio and transcription process
+    def init(self):
+        producer = threading.Thread(target=self.__producer_thread)
+        producer.start()
+
+        consumer = threading.Thread(target=self.__consumer_thread)
+        consumer.start()
     
     # Start capturing Audio from microphone
     def start(self):
@@ -44,95 +62,83 @@ class Transcribe:
     # Pauses audio capture
     def stop(self):
         self.__should_listen = False
+
+    # Returns the queue with transcribed audio
+    def get_transcriptions(self):
+        return self.__transcribed_audio
     
     # Transcribes audio clips from queue into text
     def __consumer_thread(self):
         while True:
-            if not self.__audio_queue.empty():
+            if not self.__pending_audio.empty():
+                self.__print('Transcribing audio clip...')
                 # Convert raw audio byte data into numpy array for model
-                audio_clip: np.ndarray = np.frombuffer(audio_queue.get(), np.int16).astype(np.float32) / 255.0
+                audio_clip: np.ndarray = np.frombuffer(self.__pending_audio.get(), np.int16).astype(np.float32) / 255.0
                 # Transcribe text
                 segments, info = self.__model.transcribe(audio_clip, language="en", beam_size=5)
                 # https://medium.com/@venn5708/two-important-libraries-used-for-audio-processing-and-streaming-in-python-d3b718a75904
                 user_message = ""
                 for segment in segments:
-                    #print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+                    self.__print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
                     user_message += segment.text
-                
                 # Place transcribed audio into queue
                 if (user_message != ""):
                     self.__transcribed_audio.put(user_message)
-
-
-
     
+    # Processes the audio stream and creates audio clips to be transcribed later
+    def __producer_thread(self):
+        # State variables
+        audio_data = b""
+        has_spoken = False
+        continued_silence_count = 0
+        speech_frames_count = 0
+        # Create instance of Voice Activated Detection
+        vad = webrtcvad.Vad()
+        vad.set_mode(3)
+        self.__audio_producer = pyaudio.PyAudio()
+        self.__audio_stream = self.__audio_producer.open(
+            format=pyaudio.paInt16,
+            channels=NB_CHANNELS,
+            rate=RATE,
+            input=True,
+            frames_per_buffer=CHUNK,
+        )
+        self.__print("Microphone initialized, recording started...")
 
+        while True:
+            # Read 30ms of raw audio data
+            chunk = self.__audio_stream.read(CHUNK)
+            audio_data += chunk
+            # VAD only works with 30ms audio frames
+            is_speech: bool = vad.is_speech(chunk, RATE)
 
-        
+            if is_speech:
+                if has_spoken == False:
+                    self.__print('Listening...')
+                    has_spoken = True
+                    # Removes any previous silence
+                    audio_data = chunk
+                speech_frames_count += 1
+                # Reset silence counter
+                continued_silence_count = 0
+            else:
+                # Increment on silence
+                continued_silence_count += 1
 
-
-
-def producer_thread():
-    global audio_queue, should_listen
-    # State variables
-    audio_data = b""
-    has_spoken = False
-    continued_silence_count = 0
-    speech_frames_count = 0
-    # Create instance of Voice Activated Detection
-    vad = webrtcvad.Vad()
-    vad.set_mode(3)
-    audio = pyaudio.PyAudio()
-    stream = audio.open(
-        format=pyaudio.paInt16,
-        channels=NB_CHANNELS,
-        rate=RATE,
-        input=True,
-        frames_per_buffer=CHUNK,
-    )
-    print("Microphone initialized, recording started...")
-
-    while True: 
-        # Read 30ms of raw audio data
-        chunk = stream.read(CHUNK)
-        audio_data += chunk
-        # VAD only works with 30ms audio frames
-        is_speech: bool = vad.is_speech(chunk, RATE)
-
-        if is_speech:
-            if has_spoken == False:
-                #print('Listening...')
-                has_spoken = True
-                # Removes any previous silence
-                audio_data = chunk
-            speech_frames_count += 1
-            # Reset silence counter
-            continued_silence_count = 0
-        else:
-            # Increment on silence
-            continued_silence_count += 1
-
-        # Close off audio clip after silence is detected
-        if (has_spoken and continued_silence_count >= SILENCE_LENGTH):
-            # Store audio clip in queue
-            if (should_listen and speech_frames_count >= SPEECH_MIN_LENGTH):
-                print('Saving into audio queue...')
-                audio_queue.put(audio_data)
-            # Reset counters
-            continued_silence_count = 0
-            speech_frames_count = 0
-            # Reset speech detection
-            has_spoken = False
-
-if __name__ == "__main__":
-
-    print('Downloading Whisper model...')
-
-    model_size = "base.en"
-    model = WhisperModel(model_size, device="cpu", compute_type="int8")
-
-    producer = threading.Thread(target=producer_thread)
-    producer.start()
-
-    consumer = threading.Thread(target=consumer_thread, args=[model])
-    consumer.start()
+            # Close off audio clip after silence is detected
+            if (has_spoken and continued_silence_count >= SILENCE_LENGTH):
+                self.__print("About to save audio clip...")
+                # Store audio clip in queue
+                if (self.__should_listen and speech_frames_count >= SPEECH_MIN_LENGTH):
+                    self.__print('Saving into audio queue...')
+                    self.__pending_audio.put(audio_data)
+                # Reset counters
+                continued_silence_count = 0
+                speech_frames_count = 0
+                # Reset speech detection
+                has_spoken = False
+    
+    # Prints out a debugging message
+    def __print(self, message):
+        if self.__debug_on:
+            print(message)
