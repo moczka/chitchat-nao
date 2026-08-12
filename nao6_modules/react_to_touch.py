93a71b931648b8e8af24088b7c17debeafe4b0e4
moczka <steven.guesp@gmail.com>
2026-08-12 02:13:13 -0400

    Create helper function to generate prompt from touch events

--- nao6_modules/react_to_touch.py
+++ nao6_modules/react_to_touch.py
@@ -6,7 +6,19 @@
 
 '''
 import functools
+import re
 from language_model.model import generate_response
+
+PART_LABEL_TO_NAME = {
+    "Head": "head",
+    "LArm": "left arm",
+    "RArm": "right arm",
+    "LHand": "left hand",
+    "RHand": "right hand",
+    "LFoot": "left foot",
+    "RFoot": "right foot",
+    "ChestBoard": "chest"
+}
 
 class ReactToTouch():
     def __init__(self, session):
@@ -17,20 +29,31 @@ class ReactToTouch():
         self.touch = self.memory_service.subscriber("TouchChanged")
         self.id = self.touch.signal.connect(functools.partial(self.__on_touched, "TouchChanged"))
 
-    def __on_touched(self, event_name, value):
+    def __on_touched(self, event_name, body_parts):
         # Disconnect to the event when talking, to avoid repetitions
         print("On touch was called.")
         self.touch.signal.disconnect(self.id)
         # TODO: Use http://doc.aldebaran.com/2-8/family/nao_technical/contact-sensors_naov6.html#naov6-contact-hand
         # To compose a string that can be used to generate a response from the language model.
-        print(value)
-        # for body_part, was_touched in value:
-        #     if was_touched:
-        #         # Solicit a response from the robot
-        #         self.animated_speech.say(generate_response(f"I am rubbing your {body_part} gently."))
-        #         break
-        # Reconnect to handle other touch events
+        print(body_parts)
+        for body_part in body_parts:
+            if body_part[1]:
+                # Solicit a response from the robot
+                self.animated_speech.say(self.__generate_response(body_part))
+                break
+        #Reconnect to handle other touch events
         self.id = self.touch.signal.connect(functools.partial(self.__on_touched, "TouchChanged"))
+
+    def __generate_response(body_part_info):
+        info = body_part_info.split("/")
+        # Gather the information regarding the body part
+        part_label = info[1]
+        part_location = info[2]
+        # Compose prompt for language model
+        verb = "grabbing" if re.search('Arm', part_label) else "rubbing"
+        prompt = f"I am {verb} your {PART_LABEL_TO_NAME[part_label]} in the {part_location} gently."
+        # Generate response
+        return generate_response(prompt)
 
     def pause_reactions(self):
         '''Pauses responding to touch interactions.'''
